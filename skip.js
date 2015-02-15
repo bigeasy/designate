c45d41b982793997a9875a745635b2529274dde7
Alan Gutierrez <alan@prettyrobots.com>
2015-02-14 22:57:10 -0600

    Finish upgrade to Cadence Redux.
    See #74.

--- skip.js
+++ skip.js
@@ -1,4 +1,4 @@
-var cadence = require('cadence')
+var cadence = require('cadence/redux')
 var riffle = require('riffle')
 
 function Forward (comparator, versions, iterator, record, visited, key, size) {
